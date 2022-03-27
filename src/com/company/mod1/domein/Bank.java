296f31a8de9c273597c72be914c3155025b41ea2
VagharshakGrigoryan <vaghogrig777@gmail.com>
2022-03-27 20:23:52 +0400

    use static const

--- src/com/company/mod1/domein/Bank.java
+++ src/com/company/mod1/domein/Bank.java
@@ -1,26 +1,31 @@
 package com.company.mod1.domein;
 
 public class Bank {
-    private static final Customer[] customer =  new Customer[1000];
-    private static int numOfClients =0;
+    private final Customer[] customer = new Customer[1000];
+    private int numOfClients = 0;
+    private static final Bank myBank = new Bank();
 
     private Bank() {
-
     }
 
-    public static Customer getCustomer(int customerNumber) {
+
+    public Customer getCustomer(int customerNumber) {
         if (customerNumber < customer.length) {
             return customer[customerNumber];
         }
         return null;
     }
 
-    public static void addCustomer(Customer newCustomer) {
+    public void addCustomer(Customer newCustomer) {
         customer[numOfClients] = newCustomer;
         numOfClients++;
     }
 
-    public static int getNumOfClients() {
+    public int getNumOfClients() {
         return numOfClients;
+    }
+
+    public static Bank getBank() {
+        return new Bank();
     }
 }
--- src/com/company/mod1/test/TestAccount.java
+++ src/com/company/mod1/test/TestAccount.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 public class TestAccount {
     public static void main(String[] args) {
 
+        Bank bank = Bank.getBank();
 
         Customer firstCustomer = new Customer("John Doe");
         Customer secondCustomer = new Customer("Jane Doe");
@@ -20,20 +21,23 @@ public class TestAccount {
         secondCustomer.addAccount(janeAccount);
 
 
-        Bank.addCustomer(firstCustomer);
-        Bank.addCustomer(secondCustomer);
+        bank.addCustomer(firstCustomer);
+        bank.addCustomer(secondCustomer);
 
-        System.out.println(Bank.getCustomer(0));
-        System.out.println(Bank.getCustomer(1));
+        extracted(bank);
 
-        Objects.requireNonNull(Bank.getCustomer(0)).getAccount(0).deposit(2000);
-        Objects.requireNonNull(Bank.getCustomer(0)).getAccount(1).withDraw(5500);
-        ((SavingsAccount) Objects.requireNonNull(Bank.getCustomer(0)).getAccount(0)).addInterestRate();
-
-
-        System.out.println(Bank.getCustomer(0));
-        System.out.println((Bank.getCustomer(1)));
+        Objects.requireNonNull(bank.getCustomer(0)).getAccount(0).deposit(2000);
+        Objects.requireNonNull(bank.getCustomer(0)).getAccount(1).withDraw(5500);
+        ((SavingsAccount) Objects.requireNonNull(bank.getCustomer(0)).getAccount(0)).addInterestRate();
 
 
+        System.out.println(bank.getCustomer(0));
+        System.out.println((bank.getCustomer(1)));
+
+    }
+
+    private static void extracted(Bank bank) {
+        System.out.println(bank.getCustomer(0));
+        System.out.println(bank.getCustomer(1));
     }
 }
